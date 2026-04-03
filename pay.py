108c01e5ee06705e11804aa8e89ca1aad8929baa
aasaasaas-afk <aadiimoga2009@gmail.com>
2026-04-03 18:48:24 +0530

    Update pay.py

--- pay.py
+++ pay.py
@@ -19,14 +19,19 @@ def create_payment(user_id: str, pay_currency: str):
     data = {
         "price_amount": 1,
         "price_currency": "usd",
-        "pay_currency": pay_currency,  # usdttrc20 / usdtbep20
+        "pay_currency": pay_currency,
         "order_id": user_id,
         "order_description": "Test Payment $1"
     }
 
     try:
         r = requests.post(url, json=data, headers=HEADERS)
+
+        print("STATUS CODE:", r.status_code)
+        print("RESPONSE:", r.text)  # 🔥 IMPORTANT
+
         return r.json()
+
     except Exception as e:
         print("Payment creation error:", e)
         return None
